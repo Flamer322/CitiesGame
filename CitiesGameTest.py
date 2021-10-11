3e0829e04f3d43e65b1b3bff627cb010452c315e
Flamer322 <b.bar1enev@gmail.com>
2021-10-11 18:13:25 +0300

    Added Watch tests

--- CitiesGameTest.py
+++ CitiesGameTest.py
@@ -1,10 +1,15 @@
-import pytest
-from CitiesGame import CitiesDict
+import pytest, time
+from CitiesGame import CitiesDict, Watch
 
 
 @pytest.fixture
 def cities_dict():
     return CitiesDict()
+
+
+@pytest.fixture
+def watch():
+    return Watch(2)
 
 
 def test_is_city(cities_dict):
@@ -38,3 +43,47 @@ def test_get_next_char(cities_dict):
     assert cities_dict.get_next_char('Чаны') == 'Н'
     assert cities_dict.get_next_char('Пермь') == 'М'
     assert cities_dict.get_next_char('Калуга') == 'А'
+
+
+def test_next_turn(watch):
+    assert watch.next_turn(5) == 1
+    assert watch.next_turn(5) == 2
+    assert watch.next_turn(5) == 3
+    assert watch.next_turn(5) == 4
+    assert watch.next_turn(5) == 5
+
+
+def test_get_turn_time(watch):
+    watch.start_stopwatch()
+    assert watch.get_turn_time() == 0
+    time.sleep(1)
+    assert watch.get_turn_time() == 1
+    time.sleep(0.5)
+    assert watch.get_turn_time() == 0.5
+    time.sleep(1)
+    assert watch.get_turn_time() == 1
+    assert watch.get_turn_time() == 0
+
+
+def test_turn_time(watch):
+    assert watch.turn_time == 10
+    watch.next_turn(5)
+    assert watch.turn_time == 10.1
+    watch.next_turn(5)
+    assert watch.turn_time == 10.2
+    watch.next_turn(5)
+    assert watch.turn_time == 10.3
+    watch.next_turn(5)
+    assert watch.turn_time == 10.4
+
+
+def test_average_turn_time(watch):
+    assert watch.average_turn_times == [0, 0]
+    watch.next_turn(5)
+    assert watch.average_turn_times == [5, 0]
+    watch.next_turn(6)
+    assert watch.average_turn_times == [5, 6]
+    watch.next_turn(7)
+    assert watch.average_turn_times == [6, 6]
+    watch.next_turn(8)
+    assert watch.average_turn_times == [6, 7]
